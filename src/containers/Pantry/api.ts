81472863975433af113d5ec615dc0cb49a948f37
Domantas Andrijauskas <domantas.andrijauskas@kilo.health>
2022-04-13 11:16:25 +0300

    Remove supabase

--- src/containers/Pantry/api.ts
+++ src/containers/Pantry/api.ts
@@ -1,22 +1,12 @@
 import axios from 'axios';
-import { supabase } from '../../utilities/supabase';
 
 export const getScannedItemDetails = (barcode: string) =>
   axios.get(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`);
 
 export const getPantryItems = async () => {
-  const userId = supabase.auth.user()?.id;
-
-  const data = await supabase
-    .from('pantry_items')
-    .select('*')
-    .eq('user_id', userId);
-
-  return data;
+  return [];
 };
 
 export const addPantryItem = async (data: any) => {
-  const userId = supabase.auth.user()?.id;
-
-  await supabase.from('pantry_items').insert({ ...data, user_id: userId });
+  console.log('here');
 };
