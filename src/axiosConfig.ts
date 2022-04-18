f60fba321b864ccb19831419e92f11e2513f3160
Domantas Andrijauskas <domantas.andrijauskas@kilo.health>
2022-04-18 16:16:34 +0300

    add detox + firebase (for ios)

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
 
 export const axiosClient = axios.create({
-  baseURL: 'http://10.0.2.2:8000',
+  baseURL:
+    Platform.OS === 'ios' ? 'http://localhost:8000/' : 'http://10.0.2.2:8000',
   headers: {
     'Content-Type': 'application/json',
   },
